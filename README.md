a13d717cc427b5f7ffc91b4ec22a13b1022c7fe0
Jared Cook <jcook3701@gmail.com>
2020-11-24 20:28:55 -0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Teknic Motor Controller
 
+## Design
+1. The current design is to take the output from either move_base or key_teleop (cmd_vel - geometry_msg/twist) and convert that into motor velocities for the left and right side motors.  
+
+__Note:__ When we use move_base (nav_ros - TrajectoryPlannerROS) we will have to make sure that the linear velocity and acceleration parameters (min and max) correlate correctly with the motor angular velocity and acceleration parameters(max).  
+
+## Configuration Files
+
+__Note:__  The following assumptions are being made when using this motor controller.  
+1. when using only two motors chains are hooked up that the vehicle has two chains per motor.  
+2. Motor orientation is the following: (L)  (R)  
+                                     ---[]  []---  
+__Vehicle Parameters:__  
+1. Number of Motors
+   - Number of motors being used on your vehicle.  
+2. Gear Ratio
+   - Gear ratio between motor and single tire rotation.  
+4. max velocity
+   - Max velocity that the motor may reach.  
+5. max acceleration
+   - Max acceleration that the motor may reach.  
+6. max torque
+   - Max torque that the motor may reach.  
--- src/trash_bot_motor_controller/trash_bot_motor_controller.cpp
+++ src/trash_bot_motor_controller/trash_bot_motor_controller.cpp
@@ -1,7 +1,6 @@
 //Required include files
 #include "ros/ros.h"
-
-#include "std_msgs/String.h"
+#include "ros/console.h"
 #include <geometry_msgs/Twist.h>
 
 #include <stdio.h>	
@@ -11,62 +10,60 @@
 using namespace sFnd;	
 
 
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+void cmdVelCallback(const geometry_msgs::Twist& msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  if (msg.angular.z > 0){
+    ROS_INFO_STREAM("Subscriber velocities:"<<" linear="<<msg.linear.x<<" angular="<<msg.angular.z);
+  }
 }
 
 
 int main(int argc, char* argv[]){
+  ros::init(argc, argv, "trash_bot_motor_controller");
+  ros::NodeHandle nh("~");
+  ros::Subscriber cmd_vel_sub = nh.subscribe("cmd_vel", 1000, cmdVelCallback);
 
-  ros::init(argc, argv, "listener");
-  ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  int max_motor_velocity;
+  int max_motor_acceleration;
+  int max_motor_torque;
+
+  std::vector<int> gear_ratio;
+  int number_of_motors;
   
-  size_t portCount = 0;
-  std::vector<std::string> comHubPorts;  
-  SysManager myMgr; //Create System Manager myMgr
+  if(!nh.getParam("motor_parameters/max_motor_velocity", max_motor_velocity)) {
+    nh.setParam("motor_parameters/max_motor_velocity", 0);
+    nh.getParam("motor_parameters/max_motor_velocity", max_motor_velocity);
+  }
+
+  if(!nh.getParam("motor_parameters/max_motor_acceleration", max_motor_acceleration)) {
+    nh.setParam("motor_parameters/max_motor_acceleration", 0);
+    nh.getParam("motor_parameters/max_motor_acceleration", max_motor_acceleration);
+  }
   
-  try
-    { 
-      
-      SysManager::FindComHubPorts(comHubPorts);
-      printf("Found %d SC Hubs\n", comHubPorts.size());
-      
-      for (portCount = 0; portCount < comHubPorts.size() && portCount < NET_CONTROLLER_MAX; portCount++) {
-	// define the first SC Hub port (port 0) to be associated 
-	// with COM portnum (as seen in device manager)
-	myMgr.ComHubPort(portCount, comHubPorts[portCount].c_str());
-      }
-      
-      if (portCount > 0) {
-	//printf("\n I will now open port \t%i \n \n", portnum);
-	myMgr.PortsOpen(portCount); //Open the port
-	
-	for (size_t i = 0; i < portCount; i++) {
-	  IPort &myPort = myMgr.Ports(i);
-	  
-	  printf(" Port[%d]: state=%d, nodes=%d\n",
-		 myPort.NetNumber(), myPort.OpenState(), myPort.NodeCount());
- 	}
-	// Close down the ports
-	myMgr.PortsClose();	
-      }
-      else {
-	printf("Unable to locate SC hub port\n");	
-	return -1; //This terminates the main program
-      }
-    }			
-  catch(mnErr& theErr) //This catch statement will intercept any error from the Class library
-    {
-      printf("Port Failed to open, Check to ensure correct Port number and that ClearView is not using the Port\n");	
-      //This statement will print the address of the error, the error code (defined by the mnErr class), 
-      //as well as the corresponding error message.
-      printf("Caught error: addr=%d, err=0x%08x\nmsg=%s\n", theErr.TheAddr, theErr.ErrorCode, theErr.ErrorMsg);          
-      return -1; //This terminates the main program
-    }
+  if(!nh.getParam("motor_parameters/max_motor_torque_percentage", max_motor_torque)) {
+    nh.setParam("motor_parameters/max_motor_torque_percentage", 0);
+    nh.getParam("motor_parameters/max_motor_torque_percentage", max_motor_torque);
+  }
+
+  if(!nh.getParam("vehicle_parameters/number_of_motors", number_of_motors)) {
+    nh.setParam("vehicle_parameters/number_of_motors", 2);
+    nh.getParam("vehicle_parameters/number_of_motors", number_of_motors);
+  }  
+
+  if(!nh.getParam("vehicle_parameters/gear_ratio", gear_ratio)) {
+    nh.setParam("vehicle_parameters/gear_ratio", 0);
+    nh.getParam("vehicle_parameters/gear_ratio",  gear_ratio);
+  }
 
 
+  
+  ROS_INFO_STREAM("max_motor_velocity: " << max_motor_velocity);
+  ROS_INFO_STREAM("max_motor_acceleration: " << max_motor_acceleration);
+  ROS_INFO_STREAM("max_motor_torque: " << max_motor_torque);
+
+  ROS_INFO_STREAM("number_of_motors: " << number_of_motors);
+  ROS_INFO_STREAM("gear_ratio: " << gear_ratio[0] << ":" << gear_ratio[1]);
+  
   ros::spin();
   
   return 0; //End program
